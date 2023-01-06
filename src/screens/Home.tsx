3f12e509a2653e9610926a46d15f286a16a3184f
mgckaled <marcelgckaled@gmail.com>
2023-01-06 10:58:37 -0300

    feat: add section list to Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,12 @@
-import { HStack, Icon, Text, VStack, useTheme } from 'native-base'
+import {
+	HStack,
+	Heading,
+	Icon,
+	SectionList,
+	Text,
+	VStack,
+	useTheme
+} from 'native-base'
 import { ForkKnife, Plus } from 'phosphor-react-native'
 import { useState } from 'react'
 
@@ -10,6 +18,17 @@ import { UserImage } from '@components/UserImage'
 export function Home() {
 	const { colors } = useTheme()
 	const [userPhoto, setUserPhoto] = useState('https://github.com/mgckaled.png')
+
+	const [meals, setMeals] = useState([
+		{
+			title: '12.08.22',
+			data: ['X-Tudo', 'Sanduíche', 'Lasanha de Frango com queijo']
+		},
+		{
+			title: '11.08.22',
+			data: ['X-Tudo', 'Torta de chocolate']
+		}
+	])
 
 	return (
 		<VStack flex={1} bg="gray.100" safeArea>
@@ -24,7 +43,7 @@ export function Home() {
 				<UserImage source={{ uri: userPhoto }} alt="Imagem do usuário" />
 			</HStack>
 			<VStack flex={1} mt={8} mx={8}>
-				<StatisticBox mb={6} percent="88,99" />
+				<StatisticBox flex={1} mb={6} percentMeal={88.99} colorIcon={''} />
 				<Text mb={1} fontFamily="body" fontSize="md">
 					Refeições
 				</Text>
@@ -38,7 +57,32 @@ export function Home() {
 						/>
 					}
 				/>
-				<MealCard hour={'12:00'} title={'X-Tudo'} color={colors.green[400]} />
+				<SectionList
+					flex={1}
+					mt={3}
+					mb={2}
+					sections={meals}
+					keyExtractor={item => item}
+					renderItem={({ item }) => (
+						<MealCard hour={'12:00'} title={item} color={colors.green[400]} />
+					)}
+					renderSectionHeader={({ section }) => (
+						<Heading mt={3} color="gray.600" fontSize="lg" fontFamily="heading">
+							{section.title}
+						</Heading>
+					)}
+					ListEmptyComponent={() => (
+						<Text
+							color="gray.600"
+							textAlign="center"
+							fontFamily="body"
+							fontSize="lg"
+						>
+							Não há registradas ainda. {'\n'}
+							Vamos fazer exercícios hoje?
+						</Text>
+					)}
+				/>
 			</VStack>
 		</VStack>
 	)
