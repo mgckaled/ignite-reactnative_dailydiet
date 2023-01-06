01afbf6d2b54d72cad28eb93a7053f990d34b81d
mgckaled <marcelgckaled@gmail.com>
2023-01-06 13:28:47 -0300

    feat: install nativigation dependencies and add global types

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+	namespace ReactNavigation {
+		interface RootParamList {
+			home: undefined
+			statistics: undefined
+			create: undefined
+			feedback: undefined
+			meal: undefined
+		}
+	}
+}
