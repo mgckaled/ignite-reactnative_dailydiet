873a1ca53e5b88b7fe5aca7e70677bdd7fed4209
mgckaled <marcelgckaled@gmail.com>
2023-01-06 21:24:19 -0300

    feat: install dependencies and add controll forms to CreateMeal screen

--- src/screens/CreateMeal.tsx
+++ src/screens/CreateMeal.tsx
@@ -1,7 +1,26 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useNavigation } from '@react-navigation/native'
-import { VStack } from 'native-base'
+import { HStack, ScrollView, Text, VStack } from 'native-base'
+import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
+
+type FormDataProps = {
+	name: string
+	description: string
+	date: string
+	hour: string
+}
+
+const createMealRegister = yup.object({
+	name: yup.string().required('Informe o nome da refeição'),
+	description: yup.string().required('Informe a descrição da refeição'),
+	date: yup.string().required('Informe a data da refeição'),
+	hour: yup.string().required('Informe a hora da refeição.')
+})
 
 export function CreateMeal() {
 	const navigation = useNavigation()
@@ -10,13 +29,97 @@ export function CreateMeal() {
 		navigation.goBack()
 	}
 
+	const {
+		control,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<FormDataProps>({
+		resolver: yupResolver(createMealRegister)
+	})
+
 	return (
 		<VStack flex={1}>
-			<ScreenHeader
-				onPress={handleGoBack}
-				headerBgColor="gray.300"
-				title="Nova Refeição"
-			/>
+			<VStack flex={1}>
+				<ScreenHeader
+					onPress={handleGoBack}
+					headerBgColor="gray.300"
+					title="Nova Refeição"
+				/>
+			</VStack>
+			<ScrollView
+				mx={7}
+				mb={10}
+				contentContainerStyle={{ flexGrow: 1 }}
+				showsVerticalScrollIndicator={false}
+			>
+				<VStack flex={1}>
+					<Text color="gray.700" fontSize="md" fontFamily="body" mb={1}>
+						Nome
+					</Text>
+					<Controller
+						control={control}
+						name="name"
+						render={({ field: { onChange, value } }) => (
+							<Input
+								onChangeText={onChange}
+								value={value}
+								errorMessage={errors.name?.message}
+							/>
+						)}
+					/>
+
+					<Text color="gray.700" fontSize="md" fontFamily="body" mb={1}>
+						Descrição
+					</Text>
+					<Controller
+						control={control}
+						name="description"
+						render={({ field: { onChange, value } }) => (
+							<Input
+								h={32}
+								onChangeText={onChange}
+								value={value}
+								errorMessage={errors.description?.message}
+							/>
+						)}
+					/>
+					<HStack flex={1}>
+						<VStack flex={1} mr={2}>
+							<Text color="gray.700" fontSize="md" fontFamily="body" mb={1}>
+								Data
+							</Text>
+							<Controller
+								control={control}
+								name="date"
+								render={({ field: { onChange, value } }) => (
+									<Input
+										onChangeText={onChange}
+										value={value}
+										errorMessage={errors.date?.message}
+									/>
+								)}
+							/>
+						</VStack>
+						<VStack flex={1} ml={2}>
+							<Text color="gray.700" fontSize="md" fontFamily="body" mb={1}>
+								Hora
+							</Text>
+							<Controller
+								control={control}
+								name="hour"
+								render={({ field: { onChange, value } }) => (
+									<Input
+										onChangeText={onChange}
+										value={value}
+										errorMessage={errors.hour?.message}
+									/>
+								)}
+							/>
+						</VStack>
+					</HStack>
+				</VStack>
+				<Button title="Cadastrar Refeição" />
+			</ScrollView>
 		</VStack>
 	)
 }
